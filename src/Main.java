0d58222b726e22f5a299b99d3a7a736c944d5755
konstantin <mom.konstantin@gmail.com>
2023-01-14 01:54:26 +0300

    task 1-2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1 () {
-
+        System.out.println("Задача1");
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+    public static void task2 () {
+        System.out.println("Задача2");
+        int clientOS = 0;
+        int clientDeviceYear = 2000;
+        if (clientOS == 0) {
+            if (clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
+        if (clientOS == 1) {
+            if (clientDeviceYear <= 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
     }
 }
