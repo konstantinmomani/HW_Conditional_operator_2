afbe1c525e6999e462e36780c1f7a7f7a1cc997b
konstantin <mom.konstantin@gmail.com>
2023-01-14 02:46:01 +0300

    task 1,2,4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
     }
     public static void task1 () {
         System.out.println("Задача1");
@@ -30,5 +32,23 @@ public class Main {
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
         }
+    }
+    public static void task3 () {
+        System.out.println("Задача3");
+
+    }
+    public static void task4 () {
+        System.out.println("Задача4");
+        int deliveryDistance = 100;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (deliveryDistance > 60 && deliveryDistance < 100) {
+            System.out.println("Потребуется дней: 3");
+        } else {
+            System.out.println("Доставки нет");
+        }
+
     }
 }
